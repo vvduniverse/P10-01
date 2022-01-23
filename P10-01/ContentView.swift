ea9345d38040067e753661f5e3e432bec445ffc4
Vahtee Boo <vahtee.boo@gmail.com>
2022-01-23 13:54:45 +0200

    Initial Commit

---
+++ P10-01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P10-01
+//
+//  Created by Vahtee Boo on 23.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ P10-01/P10_01App.swift
@@ -0,0 +1,17 @@
+//
+//  P10_01App.swift
+//  P10-01
+//
+//  Created by Vahtee Boo on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct P10_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
